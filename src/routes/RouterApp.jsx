504002b90f167cfbd390838623dc9ba274cbeca7
Nguyễn Đức Hoài Nam <namnguyen09042001@gmail.com>
2023-11-01 00:29:09 +0700

    fix-routes

--- src/routes/RouterApp.jsx
+++ src/routes/RouterApp.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import { HomePage } from '../pages/HomePage';
+// import { HomePage } from '../pages/HomePage';
 import SigninPage from '../pages/SigninPage';
 import StudentPage from '../pages/student/studentPage';
 
@@ -9,8 +9,8 @@ function RouterApp() {
     return (
         <Router>
             <Routes>
-                <Route path="/signin" element={<SigninPage />} />
-                <Route path="/" element={<HomePage />} />
+                {/* <Route path="/signin" element={<SigninPage />} /> */}
+                <Route path="/" element={<SigninPage />} />
                 <Route path="/student" element={<StudentPage />} />
             </Routes>
         </Router>
