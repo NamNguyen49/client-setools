8108503992f92e047e27bfe4368284a20d3db5c9
Nguyễn Đức Hoài Nam <namnguyen09042001@gmail.com>
2023-11-06 02:54:55 +0700

    donefe2

--- src/pages/Admin/ListUser.jsx
+++ src/pages/Admin/ListUser.jsx
@@ -167,7 +167,7 @@ export default function ListUser() {
                 aria-label="menu"
                 sx={{ mr: 2 }}
               >
-                <MenuIcon />
+
               </IconButton>
               <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
                 SE TOOL
