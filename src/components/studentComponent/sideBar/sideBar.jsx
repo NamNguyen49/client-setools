ab8ad7421e28bfccbdcc05532ef9686e7d7e9c80
Nguyễn Đức Hoài Nam <namnguyen09042001@gmail.com>
2023-11-05 21:34:19 +0700

    update

--- src/components/studentComponent/sideBar/sideBar.jsx
+++ src/components/studentComponent/sideBar/sideBar.jsx
@@ -1,43 +1,4 @@
 
-// import React from 'react';
-// import './sideBar.css';
-// import { SidebarData } from './sideBarData';
-
-// import Modalpopup from '../../CreateProject';
-
-// function SideBar({ openDrawer }) {
-//   const data = SidebarData(); // Gọi SidebarData như một hàm để lấy danh sách dữ liệu
-
-//   return (
-//     <div className='SideBar'>
-//       <ul className='SideBarList'>
-//         {data.map((value, key) => { // Sử dụng data thay vì SidebarData
-//           return (
-//             <li id={window.location.pathname === value.link ? "Active" : ""} className='row' key={key}
-//               onClick={() => {
-//                 window.location.pathname = value.link;
-//                 openDrawer(false);
-//               }}
-//             >
-//               <div id='icon'>
-//                 {value.icon}
-//               </div>
-//               <div id='title'>{value.title}</div> {/* Sửa value.titile thành value.title */}
-//             </li>
-//           );
-//         })}
-
-//         <li className='col'>
-//           <div id='icon'>
-//             <Modalpopup />
-//           </div>
-//         </li>
-//       </ul>
-//     </div>
-//   );
-// }
-
-// export default SideBar;
 import React from 'react';
 import './sideBar.css';
 import { SidebarData } from './sideBarData';
